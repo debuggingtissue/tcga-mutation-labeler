1fff11e9ac279ea991fa89308b96a2be757b0923
debuggingtissue <debugger@debuggingtissue.com>
2020-01-08 22:33:27 +0100

    add MVP implementation

--- src/label_cases.py
+++ src/label_cases.py
@@ -1,8 +1,9 @@
 import sys
 import csv
-from os import listdir
-from os.path import isfile, join, isdir
+from os import listdir, makedirs, _exit
+from os.path import isfile, join, isdir, exists
 import ntpath
+from shutil import copy, move
 
 ntpath.basename("a/b/c")
 
@@ -51,27 +52,91 @@ def get_path_leaf(path):
     return tail or ntpath.basename(head)
 
 
-def label_images(all_image_paths):
-    for image_path in all_image_paths:
-        print(get_path_leaf(image_path))
+def label_images(all_image_paths, case_ids_with_mutation):
+    mutation_true, mutation_false = [], []
+    for case_id_with_mutation in case_ids_with_mutation:
+        case_id_with_mutation_without_sample_vial = case_id_with_mutation[:-1]
+        for image_path in all_image_paths:
+            image_file = get_path_leaf(image_path)
+            if case_id_with_mutation_without_sample_vial in image_file:
+                mutation_true.append(image_path)
+
+    mutation_false = [item for item in all_image_paths if item not in mutation_true]
+    return mutation_true, mutation_false
+
+
+def confirm_output_directory(output_directory_path):
+    if not exists(output_directory_path):
+        makedirs(output_directory_path)
+    return output_directory_path
+
+
+def generate_output_path(output_root_directory_path, mutation_of_interest, has_mutation):
+    if has_mutation:
+        return confirm_output_directory(output_root_directory_path + "/" + mutation_of_interest + "_true")
+    else:
+        return confirm_output_directory(output_root_directory_path + "/" + mutation_of_interest + "_false")
+
+
+def copy_images(source_image_paths, destination_directory_path):
+    for source_image_path in source_image_paths:
+        copy(source_image_path, destination_directory_path)
+
+
+def move_images(source_image_paths, destination_directory_path):
+    for source_image_path in source_image_paths:
+        move(source_image_path, destination_directory_path)
 
 
 if __name__ == '__main__':
-    print(sys.argv)
+    print("\n")
+    # print(sys.argv)
     tcga_cases_directory_path = sys.argv[1]
     mutation_file_path = sys.argv[2]
     mutation_of_interest = sys.argv[3]
-    output_path = sys.argv[4]
+    output_root_directory_path = sys.argv[4]
+    desired_disk_operation = sys.argv[5]
 
     case_ids_with_mutation = get_case_ids_with_mutation(mutation_of_interest)
     case_directory_paths = get_subdirectory_paths(tcga_cases_directory_path)
     all_image_paths = get_all_image_paths(case_directory_paths)
-    mutation_true, mutation_false = label_images(all_image_paths)
 
-    print(case_ids_with_mutation)
-    print(get_subdirectory_paths(tcga_cases_directory_path))
-    print(all_image_paths)
+    print("===========")
+    print("Labeling mutations")
+    print("===========\n")
 
-    print(tcga_cases_directory_path)
-    print(mutation_file_path)
-    print(output_path)
+    mutation_true, mutation_false = label_images(all_image_paths, case_ids_with_mutation)
+
+    print("===========")
+    print("Number of cases with " + mutation_of_interest + " mutation " + str(len(mutation_true)))
+    print("Number of cases without " + mutation_of_interest + " mutation " + str(len(mutation_false)))
+    print("===========\n")
+
+    output_path_mutation_true = generate_output_path(output_root_directory_path, mutation_of_interest, True)
+    output_path_mutation_false = generate_output_path(output_root_directory_path, mutation_of_interest, False)
+
+    if desired_disk_operation == "-copy":
+        print("===========")
+        print("Copying files to " + output_root_directory_path)
+        print("===========\n")
+
+        copy_images(mutation_true, output_path_mutation_true)
+        copy_images(mutation_false, output_path_mutation_false)
+    elif desired_disk_operation == "-move":
+
+        print("===========")
+        print("Moving files to " + output_root_directory_path)
+        print("===========\n")
+
+        move_images(mutation_true, output_path_mutation_true)
+        move_images(mutation_false, output_path_mutation_false)
+    else:
+        print("===========")
+        print("Non valid disk operation selected, please use -copy or -move")
+        print("===========\n")
+
+        _exit(1)
+
+    print("===========")
+    print("TCGA mutation labeling has succeeded")
+    print("===========\n")
--- src/run_labeler.sh
+++ src/run_labeler.sh
@@ -4,5 +4,6 @@ TCGA_CASES_DIRECTORY_PATH=${1?Error: no TCGA cases directory path given}
 MUTATION_FILE_PATH=${2?Error: no mutation file path given}
 MUTATION_OF_INTEREST=${3?Error: no mutation of interest given}
 OUTPUT_DIRECTORY_PATH=${4?Error: no output directory path given}
+DESIRED_DISK_OPERATION=${5?Error: no desired disk operation given, please select -move or -copy}
 
-python label_cases.py $TCGA_CASES_DIRECTORY_PATH $MUTATION_FILE_PATH $MUTATION_OF_INTEREST $OUTPUT_DIRECTORY_PATH
+python label_cases.py "$TCGA_CASES_DIRECTORY_PATH" "$MUTATION_FILE_PATH" "$MUTATION_OF_INTEREST" "$OUTPUT_DIRECTORY_PATH" "$DESIRED_DISK_OPERATION"
