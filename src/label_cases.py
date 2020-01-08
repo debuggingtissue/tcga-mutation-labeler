e39082d8848d1703c0a7d7a53f564b608e89b93d
debuggingtissue <debugger@debuggingtissue.com>
2020-01-08 10:00:15 +0100

    add extraction of mutation cases

--- src/label_cases.py
+++ src/label_cases.py
@@ -1,5 +1,28 @@
 import sys
+import csv
 
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv)
+
+def get_case_ids_with_mutation(mutation_of_interest):
+    with open(mutation_file_path) as tsv_file:
+        reader = csv.reader(tsv_file, delimiter='\t')
+        rows_with_mutation_of_interest = []
+        for row in reader:
+            if row[1] == mutation_of_interest:
+                rows_with_mutation_of_interest.append(row[0])
+        return rows_with_mutation_of_interest
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    tcga_cases_directory_path = sys.argv[1]
+    mutation_file_path = sys.argv[2]
+    mutation_of_interest = sys.argv[3]
+    output_path = sys.argv[4]
+
+    case_ids_with_mutation = get_case_ids_with_mutation(mutation_of_interest)
+    
+    print(case_ids_with_mutation)
+
+    print(tcga_cases_directory_path)
+    print(mutation_file_path)
+    print(output_path)
--- src/run_labeler.sh
+++ src/run_labeler.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TCGA_CASES_DIRECTORY_PATH=${1?Error: no TCGA cases directory path given}
-OUTPUT_DIRECTORY_PATH=${2?Error: no output directory path given}
+MUTATION_FILE_PATH=${2?Error: no mutation file path given}
+MUTATION_OF_INTEREST=${3?Error: no mutation of interest given}
+OUTPUT_DIRECTORY_PATH=${4?Error: no output directory path given}
 
-python label_cases.py $TCGA_CASES_DIRECTORY_PATH $OUTPUT_DIRECTORY_PATH
+python label_cases.py $TCGA_CASES_DIRECTORY_PATH $MUTATION_FILE_PATH $MUTATION_OF_INTEREST $OUTPUT_DIRECTORY_PATH
